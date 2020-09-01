f16bf620fe1f094f64571f83a484eea9e04eac5f
JeanHaiz <jean.haizmann@gmail.com>
2020-09-01 12:26:13 +0200

    flask migration script

---
+++ flask_app/manage.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
+from utils import sql_utils
+
+host = sql_utils.Host.LOCAL_JEAN   # TODO add env variable
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = sql_utils.getDBURLFromHost(host)
+
+db = SQLAlchemy(app)
+db.init_app(app)
+
+migrate = Migrate(app, db)
+migrate.init_app(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
