7cd2f7851e97ae89d4887f14560e9952105c83e8
JeanHaiz <jean.haizmann@gmail.com>
2021-01-21 15:26:44 +0100

    param update for local server

--- main/article_fetching.py
+++ main/article_fetching.py
@@ -9,7 +9,7 @@ from utils import models
 from utils import mail_utils
 from utils.data_manager import DataManager
 
-host = sql_utils.Host.G_CLOUD_SSL
+host = sql_utils.Host.LOCAL_JEAN
 schema = models.schema
 
 log_file_name = DataManager.getModulePath() + os.sep + 'main' + os.sep + 'logs' + os.sep + 'article_fetching_log.txt'
